da88e2527e56518840a35463fd72d01f9977fa33
PraveenKumar263 <PraveenKumar263>
2024-08-17 23:08:25 +0800

    edit cors origin

--- app.js
+++ app.js
@@ -2,12 +2,20 @@
 const express = require('express');
 const todoRouter = require('./routes/todoRoutes');
 const requestLogger = require('./utils/logger');
+const cors = require('cors');
 
 // create an express application
 const app = express();
 
 // use the express middleware for parsing json data
 app.use(express.json());
+
+// use the express middlerware for enabling CORS
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    optionsSuccessStatus: 200
+}))
 
 // use the express middleware for logging
 app.use(requestLogger);
